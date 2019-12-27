e7543d10bba72798ea66872c9179ccddb2843f9a
eugenejs <57704927+delictt@users.noreply.github.com>
2019-12-27 10:54:08 +0300

    fixes

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,8 @@ $(document).ready(function() {
 
 		$(this).toggleClass('is-active')
 
-		if($(this).hasClass('is-active')){
-			$('.menu').addClass('menu_visible')
-
-		}else{
-			$('.menu').removeClass('menu_visible')
-		}
+		if($(this).hasClass('is-active')) $('.menu').addClass('menu_visible')
+		else $('.menu').removeClass('menu_visible')
 
 	});
 
@@ -35,6 +31,7 @@ $(document).ready(function() {
 		touchMove: false,
 		swipe: false
 	});
+
 
 	$('.products').slick({
 		slidesToShow: 3,
@@ -72,24 +69,14 @@ $(window).resize(function (){
 
 	if($('.hamburger').hasClass('is-active')) $('.hamburger').click()
 
-
 });
 
 $(window).scroll(function (){
-	if($(this).scrollTop() >= 50){
-		$('.menu').addClass('menu_scroll')
-	}else{
-		$('.menu').removeClass('menu_scroll')
-	}
 
-	if($(this).scrollTop() >= $(window).height()){
+	if($(this).scrollTop() >= 50) $('.menu').addClass('menu_scroll')
+	else $('.menu').removeClass('menu_scroll')
 
-		$('.btn_scroll').addClass('btn_scroll_visible')
-
-	}else{
-
-		$('.btn_scroll').removeClass('btn_scroll_visible')
-
-	}
+	if($(this).scrollTop() >= $(window).height()) $('.btn_scroll').addClass('btn_scroll_visible')
+	else $('.btn_scroll').removeClass('btn_scroll_visible')
 
 });
